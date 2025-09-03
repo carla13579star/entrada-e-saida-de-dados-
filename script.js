8831836a7552e5bd2e6e0615d420256dff3e74bb
carla13579star <carla.guarani@escola.pr.gov.br>
2025-09-03 20:14:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//tipo de dados
+//tipo STRING: TEXTO. DENTIFICADO PELO USO DAS ASPAS 
+//DUPLAS
+let nome = "carla";
+
+//tipo NUMBER: se trata de número.
+
+let num = 10
+let num2 = "10";
+
+//tipo booleano: aceita dois valores como referencia e retorna
+// em dado com base na condicao.
+
+//let ave = "pinguim";
+//if (ave = true) {
+//alert ("é uma ave.")
+//} else {
+  //  alert ("não é ave.");
+//}
+
+alert("olá seja bem vindo ao meu site");
+alert("precisamos confirmar alguns dados, antes de continuar");
+let = seuNome = prompt
+ ("digite seu nome, por favor!");
+
+alert("seja bem vindo" + seuNome + "ao meu site"); //usamos concanetação
+alert(" agora precisamos saber sua idade!");
+let idade = prompt("digite sua idade!");
+alert("já sei que você tem" + idade + " agora preciso do seu e-mail!");
+let e-mail = prompt("Digite seu e-mail!");
+alert("seu e-mail é" + e-mail);
+
+console.log(nome);
+console.log(num);
+console.log(num2);
+console.log(seuNome);
+console.log(idade);
+console.log(e-mail);
